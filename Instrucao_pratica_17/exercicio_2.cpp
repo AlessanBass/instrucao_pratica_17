f57bee04658b99f393313c6b518d082db90e756f
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2023-10-10 20:03:43 -0300

    Resolução da questão do 2 da pratica 17

--- Instrucao_pratica_17/exercicio_2.cpp
+++ Instrucao_pratica_17/exercicio_2.cpp
@@ -10,11 +10,69 @@
 
 using namespace std;
 
+class Ponto {
+  private:
+    int x = 0;
+    int y = 0;
 
+
+    public:
+    //Construtor
+    Ponto (int _x = 0, int _y = 0){
+        x = _x;
+        y = _y;
+    }
+
+    void setX(int _x){
+        x = _x;
+    }
+
+    void setY(int _y){
+        y = _y;
+    }
+
+    void setCoordenadas(int _x, int _y){
+        x = _x;
+        y = _y;
+    }
+
+    int getX (){
+        return x;
+    }
+
+    int getY (){
+        return y;
+    }
+
+    double calcularDistancia(){
+        return sqrt((pow(x, 2) + pow(y,2)));
+    }
+
+};
 
 
 int main (){
 
-    
+    //Instanciando objeto
+    Ponto p1 (3,4);
+    double distancia = p1.calcularDistancia();
+
+    cout << "Reposta: a distancia do ponto (" << p1.getX() << "," << p1.getY() << ") ate a origem eh de " << distancia << endl;
+
+    Ponto p2(-2, 7);
+    p2.setCoordenadas(1,1);
+    distancia = p2.calcularDistancia();
+
+    cout << "Reposta: a distancia do ponto (" << p2.getX() << "," << p2.getY() << ") ate a origem eh de " << distancia << endl;
+
+    Ponto pontos[3];
+    pontos[0].setCoordenadas(2,2);
+    pontos[1].setCoordenadas(-1,5);
+    pontos[2].setCoordenadas(0,0);
+
+    for(int i = 0; i<3; i++){
+        distancia = pontos[i].calcularDistancia();
+        cout << "Reposta: a distancia do ponto " << i + 1 << " eh de " << distancia << endl;
+    }
     return 0;
 }
--- Instrucao_pratica_17/exercicio_3.cpp
+++ Instrucao_pratica_17/exercicio_3.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <vector>
 
+/*
+    Crie um sistema de cadastro de produtos em um supermercado. Cada grupo deve
+    projetar as classes Produto, Estoque e CarrinhoDeCompras. O Produto deve ter
+    atributos como nome, preço e código. O Estoque deve controlar a quantidade de cada
+    produto. O CarrinhoDeCompras deve permitir adicionar e remover produtos, calcular
+    o valor total e exibir o conteúdo.
+*/
+
 using namespace std;
 
 
