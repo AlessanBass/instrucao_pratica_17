06eeaf8e19cffa370134df38c2d9bd6cebc6aeb3
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2023-10-18 11:43:06 -0300

    Adiçao da classe Amizade, funcionemnto da criaçao de amizade

--- Persistencia_de_dados/contatos.cpp
+++ Persistencia_de_dados/contatos.cpp
@@ -156,12 +156,12 @@
             cout << "Infome o telefone do contato: " << endl;
             getline(cin, telefone);
             escrita << telefone;
-            escrita << " ";
+            escrita << endl;
 
-            cout << "Infome os amigos separados por virgula " << endl;
+            /* cout << "Infome os amigos separados por virgula " << endl;
             getline(cin, amigos);
             escrita << amigos;
-            escrita << endl;
+            escrita << endl; */
 
 
             escrita.close();
@@ -233,7 +233,7 @@
                 novoContato.setNome(dados[1]);
                 novoContato.setEmail(dados[2]);
                 novoContato.setTelefone(dados[3]);
-                novoContato.setAmigos(dados[4]);
+                //novoContato.setAmigos(dados[4]);
 
                 contatos.push_back(novoContato);
             }
@@ -260,7 +260,88 @@
 
     };
 
+class Amizade {
+    private: 
+     int idConato;
+     int idAmigo;
 
+     public:
+     void setIdContato(int _idContato){
+        idConato = _idContato;
+     }
+
+     void setIdAmigo(int _idAmigo){
+        idAmigo = _idAmigo;
+     }
+
+     int getIdContato(){
+        return idConato;
+     }
+
+     int getIdAmigo(){
+        return idAmigo;
+     }
+
+
+
+      vector<string> quebralinha(string _linha){
+            vector<string> vetor;
+                size_t pos = 0;
+                while ((pos = _linha.find(" ")) != string::npos) {
+                    string palavra = _linha.substr(0, pos);
+                    vetor.push_back(palavra);
+                    _linha.erase(0, pos + 1);
+                }
+                // A última palavra, após o último espaço em branco
+                vetor.push_back(_linha);
+
+                // Agora o vetor contém as palavras que estavam na linha
+            /*    for (const std::string& palavra : vetor) {
+                    std::cout << palavra << std::endl;
+                } */
+        return vetor;
+        
+    
+    }
+
+     void carregarAmizades(vector<Amizade>& a){
+        ifstream lista("amizades.txt");
+        string linha;
+        while (getline(lista, linha)) {
+            vector<string> dados = quebralinha(linha);
+            Amizade amg;
+            int id1 = stoi(dados[0]);
+            int id2 = stoi(dados[1]);
+
+            amg.setIdContato(id1);
+            amg.setIdAmigo(id2);
+
+            a.push_back(amg);
+
+        }
+
+        lista.close();
+        
+     }
+
+     void criarAmizade(){
+        ofstream escrita;
+        escrita.open("amizades.txt", ios_base::app);
+
+        cout << "Fazer amigo de: ";
+        cin >> idConato;
+        escrita << idConato;
+        escrita << " ";
+
+        cout << "Com quem ? ";
+        cin >> idAmigo;
+        escrita << idAmigo;
+        escrita << endl;
+
+
+        escrita.close();
+     }
+};
 
 
     int main(){
@@ -268,9 +349,19 @@
         vector<Contato> contatos;
         Contato c;
         Mensagem m;
+        vector<Amizade> amigos;
+        Amizade a;
         string teste;
         int idContato;
         idContato = c.carregarContatos(contatos);
+        a.carregarAmizades(amigos);
+        cout << "Tamamho de amigos: " << amigos.size() << endl;
+        /*
+           1° Mudar a abordagem de fazer amizades -ok
+           2° Talvez mudar a persistencia das amizades - ok
+           3° Verificar se uma amizade já foi criada
+
+        */
 
 
 
@@ -280,9 +371,10 @@
             cout << "[1] CRIAR CONTATO" << endl;
             cout << "[2] LISTAR CONTATOS" << endl;
             cout << "[3] LISTAR CONTATOS DE ALGUEM" << endl;
-            cout << "[4] ESCREVER UMA MENSAGEM" << endl;
-            cout << "[5] VER MENSAGENS DE UM CONTATO" << endl;
-            cout << "[6] SAIR" << endl;
+            cout << "[4] CRIAR AMIZADE" << endl;
+            cout << "[5] ESCREVER UMA MENSAGEM" << endl;
+            cout << "[6] VER MENSAGENS DE UM CONTATO" << endl;
+            cout << "[7] SAIR" << endl;
             cin >> op;
             cin.ignore();
 
@@ -294,6 +386,8 @@
                 c.setId(idContato+1);
                 c.criarContato();
                 cout << "=============================" << endl;
+
+                //OBS: Ao terminar de criar um contato carregar novamente os contatos
                 
                 break;
 
@@ -311,15 +405,11 @@
             //vector<int> amigosDoContato;
             for(int i = 0; i<contatos.size(); i++){
                 if(contatos[i].getId() == busca){ //achei quem tava procurando
-                vector<int> amigosDoContato = contatos[i].retornaAmigos(contatos[i].getAmigos()); //Retorno os amigos
-                for(int j = 0; j<amigosDoContato.size(); j++){ // Busca pelos amigos na lista de contatos
-                        for(int m = 0; m<contatos.size(); m++){
-                            if(amigosDoContato[j] == contatos[m].getId()){ //Achei um amigo
-                                cout << "Nome : " <<contatos[m].getNome() << endl;
+                    for(int j = 0; j<amigos.size(); j++){ 
+                            if(amigos[j].getIdContato() == busca){
+                                cout << "Nome : " <<contatos[amigos[j].getIdAmigo()-1].getNome() << endl;
                             }
-                        }
-                }
-                // break; pode ser necessário o uso do break aqui
+                    }
                 }
             }
 
@@ -327,6 +417,11 @@
             break;
 
             case 4:
+            cout << ">>>> CRIANDO AMIZADE <<<< " << endl;
+            a.criarAmizade();
+            break;
+
+            case 5:
             int idEscritor;
             cout << "Qual contato vai escrever?" << endl;
             cin >> idEscritor;
@@ -336,16 +431,16 @@
             m.escreverMensagem();
             break;
 
-            case 5:
+            case 6:
             //int busca;
-            
             cout << "Buscar mensagem de qual contato? "<< endl;
             cin >> teste;
             m.listarMensagens(teste);
 
             break;
 
-            case 6:
+
+            case 7:
             break;
 
             
@@ -355,7 +450,7 @@
             }
 
 
-        }while (op != 6); 
+        }while (op != 7); 
         
 
         return 0;
